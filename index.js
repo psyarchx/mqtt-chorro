2d29bde87598b100a39adfe1e13b1a45c97a139b
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2023-11-22 20:55:27 -0300

    ó toma esse lixo aqui e ajeita

--- index.js
+++ index.js
@@ -1,102 +1,152 @@
 const broker = 'wss://test.mosquitto.org:8081';
-    const topic = 'chorro';
+const topic = 'chorro';
 
-    const canvas = document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-    const img = new Image();
-    img.src = 'Annoying_Dog_sprite.webp';
-    const doggoSize = {
-      width: 10,
-      height: 10
-    };
+const img = new Image();
+img.src = 'Annoying_Dog_sprite.webp';
 
-    const safezone = {
-      x: canvas.width / 2 - 50,
-      y: canvas.height / 2 - 50,
-      width: 100,
-      height: 100
-    };
+canvas.width  = 900
+canvas.height = 900
 
-    // Set the initial position to the middle of the safezone but offset by -20
-    const initialX = safezone.x + safezone.width / 2 - doggoSize.width / 2 - 20;
-    const initialY = safezone.y + safezone.height / 2 - doggoSize.height / 2 - 20;
+// console.log(img.height);
+// console.log(img.width);
 
-    img.onload = () => {
-      // Draw the image on the canvas at the new initial coordinates
-      ctx.drawImage(img, initialX, initialY, doggoSize.width, doggoSize.height);
-    };
+const tamanhoRelativoChorro = 0.10;
 
-    // Draw green safe zone
-    ctx.fillStyle = 'green';
-    ctx.fillRect(safezone.x, safezone.y, safezone.width, safezone.height);
+const doggo = {
+	width:  img.width  * tamanhoRelativoChorro,
+	height: img.height * tamanhoRelativoChorro,
+	x: 0,
+	y: 0,
+};
 
-    // Connect to the broker
-    const client = mqtt.connect(broker);
+const safezone = {
+	width:  canvas.width  / 2 - 200,
+	height: canvas.height / 2 - 200,
 
-    // Subscribe to the topic
-    client.on('connect', () => {
-      client.subscribe(topic, (err) => {
-        if (err) {
-          console.error('Error subscribing to topic:', err);
-        } else {
-          console.log('Subscribed to topic:', topic);
-        }
-      });
-    });
+	x: canvas.width,
+	y: canvas.height,
+};
 
-    client.on('message', (receivedTopic, message) => {
-      console.log('Received message:', message.toString());
+safezone.x = canvas.width  / 2 - safezone.width / 2;
+safezone.y = canvas.height / 2 - safezone.height / 2;
 
-      // Parse the message JSON
-      try {
-        const parsedMsg = JSON.parse(message.toString());
 
-        // Check if the message object contains 'msg' property
-        if (parsedMsg.msg && (parsedMsg.msg.startsWith('n') || parsedMsg.msg.startsWith('r'))) {
-          const msg = parsedMsg.msg;
+// Set the initial position to the middle of the safezone but offset by -20
+const initialX = safezone.x + safezone.width  / 2 - doggo.width  / 2;
+const initialY = safezone.y + safezone.height / 2 - doggo.height / 2;
 
-          // Extract x and y coordinates from the message
-          const match = msg.match(/nx(-?\d+\.\d+)y(-?\d+\.\d+)/);
-          if (match && match.length === 3) {
-            const x = parseFloat(match[1]);
-            const y = parseFloat(match[2]);
 
-            // Clear the canvas
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+const client = mqtt.connect(broker);
 
-            // Redraw the green safe zone
-            ctx.fillStyle = 'green';
-            ctx.fillRect(safezone.x, safezone.y, safezone.width, safezone.height);
+// Subscribe to the topic
+client.on('connect', () => {
+	client.subscribe(topic, (err) => {
+		if (err) {
+			console.error('Error subscribing to topic:', err);
+		} else {
+			console.log('Subscribed to topic:', topic);
+		}
+	});
+});
 
-            // Check if the dog is inside the safe zone
-            ctx.drawImage(img, x, y, doggoSize.width, doggoSize.height);
-            if (x >= safezone.x && x <= safezone.x + safezone.width && y >= safezone.y && y <= safezone.y + safezone.height) {
-              // Draw the image at the new coordinates (x, y) within the safe zone
-            } else {
-              // Draw an arrow pointing towards the outside of the safe zone
-              const arrowLength = 20;
-              const arrowAngle = Math.atan2(y - (safezone.y + safezone.height / 2), x - (safezone.x + safezone.width / 2));
+client.on('message', (receivedTopic, message) => {
+	const mensagem = message.toString();
+	// console.log('Received message:', mensagem);
 
-              ctx.fillStyle = 'red';
-              ctx.beginPath();
-              ctx.moveTo(safezone.x + safezone.width / 2 + Math.cos(arrowAngle) * (safezone.width / 2 + arrowLength),
-                         safezone.y + safezone.height / 2 + Math.sin(arrowAngle) * (safezone.height / 2 + arrowLength));
-              ctx.lineTo(safezone.x + safezone.width / 2 + Math.cos(arrowAngle - Math.PI * 0.75) * arrowLength,
-                         safezone.y + safezone.height / 2 + Math.sin(arrowAngle - Math.PI * 0.75) * arrowLength);
-              ctx.lineTo(safezone.x + safezone.width / 2 + Math.cos(arrowAngle + Math.PI * 0.75) * arrowLength,
-                         safezone.y + safezone.height / 2 + Math.sin(arrowAngle + Math.PI * 0.75) * arrowLength);
-              ctx.fill();
-            }
-          }
-        } else {
-          // Clear the canvas
-          ctx.clearRect();
+	interpretaMensagem(mensagem);
 
-          // Redraw the image at the adjusted initial coordinates
-          ctx.drawImage(img, initialX + 20, initialY + 20, doggoSize.width, doggoSize.height);
-        }
-      } catch (error) {
-        console.error('Error parsing message:', error);
-      }
-    });
+	// Parse the message JSON
+	// try {
+	// 	parsaMensagem(mensagem);
+	// } catch (error) {
+	// 	console.error('Error parsing message:', error);
+	// }
+});
+
+function interpretaMensagem(message) {
+	
+	// Check if the message object contains 'msg' property
+	const ehN = message.startsWith('n');
+	const ehR = message.startsWith('r');
+	const deuCerto = message && (ehN || ehR);
+	if (!deuCerto) {
+		console.log("deu bosta");
+		return;
+	}
+
+	// Extract x and y coordinates from the message
+	const match = message.match(/.x(-?\d+\.\d+)y(-?\d+\.\d+)/);
+
+	const matchouORegexoSexo = match && match.length === 3;
+	if (!matchouORegexoSexo) {
+		console.log("deu bosta no regex");
+		return;
+	}
+
+	const x = parseFloat(match[1]);
+	const y = parseFloat(match[2]);
+
+	doggo.x = x;
+	doggo.y = y;
+
+	if (ehR) {
+		console.log(`TODO: precisa fazer o reset pra ${x}, ${y}`);
+	}	else if (ehN) {
+		console.log(`TODO: nova posicao ${x}, ${y}`);
+	}
+
+	// remove esse lixo aqui, faz ele em outro lugar
+
+	if (x >= safezone.x && x <= safezone.x + safezone.width && y >= safezone.y && y <= safezone.y + safezone.height) {
+		// checa isso no render, ao invés de x e y, usa doggo.x e doggo.y
+
+		// Draw the image at the new coordinates (x, y) within the safe zone
+	} else {
+
+		// NAO DESENHA A SETA AQUI DESENHA NO RENDER
+
+	}
+}
+
+
+
+const desenhaSetaEstranha = () => {
+	ctx.fillStyle = 'red';
+	const arrowLength = 20;
+	const arrowAngle = Math.atan2(doggo.y - (safezone.y + safezone.height / 2), doggo.x - (safezone.x + safezone.width / 2));
+
+	ctx.beginPath();
+	ctx.moveTo(safezone.x + safezone.width  / 2 + Math.cos(arrowAngle) * (safezone.width / 2 + arrowLength),
+						 safezone.y + safezone.height / 2 + Math.sin(arrowAngle) * (safezone.height / 2 + arrowLength));
+
+	ctx.lineTo(safezone.x + safezone.width  / 2 + Math.cos(arrowAngle - Math.PI * 0.75) * arrowLength,
+						 safezone.y + safezone.height / 2 + Math.sin(arrowAngle - Math.PI * 0.75) * arrowLength);
+
+	ctx.lineTo(safezone.x + safezone.width  / 2 + Math.cos(arrowAngle + Math.PI * 0.75) * arrowLength,
+						 safezone.y + safezone.height / 2 + Math.sin(arrowAngle + Math.PI * 0.75) * arrowLength);
+
+	ctx.fill();
+}
+
+const render = () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = 'green';
+	ctx.fillRect(safezone.x, safezone.y, safezone.width, safezone.height);
+
+	desenhaSetaEstranha();
+
+	ctx.drawImage(img, initialX, initialY, doggo.width, doggo.height);
+}
+
+
+img.onload = () => {
+	doggo.width  = img.width  * tamanhoRelativoChorro
+	doggo.height = img.height * tamanhoRelativoChorro
+	
+	window.requestAnimationFrame(render);
+};
+
